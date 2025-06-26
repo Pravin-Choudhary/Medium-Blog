9be5d49ac2b10f0cf9ac27ad60481183088e829b
Pravin Choudhary <pravinchoudhary.aesxa29@gmail.com>
2025-06-26 17:41:26 +0530

    Deployed npm package for Common

---
+++ common/dist/blog.d.ts
@@ -0,0 +1,29 @@
+import z from "zod";
+export declare const createBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    authorID: z.ZodNumber;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    authorID: number;
+}, {
+    title: string;
+    content: string;
+    authorID: number;
+}>;
+export declare const updateBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    id: z.ZodNumber;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    id: number;
+}, {
+    title: string;
+    content: string;
+    id: number;
+}>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
