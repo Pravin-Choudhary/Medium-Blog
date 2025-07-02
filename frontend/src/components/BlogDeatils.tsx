0ea28ae002e8c9990b8012b6fe28ca558fe335a4
Pravin Choudhary <pravinchoudhary.aesxa29@gmail.com>
2025-07-02 20:34:54 +0530

    Added Blog Deatils component

---
+++ frontend/src/components/BlogDeatils.tsx
@@ -0,0 +1,45 @@
+
+import { MultiUserAvatar } from "./MultiUserAvatar";
+
+interface BlogDeatilsType{
+    authorName : string;
+    userDp : string | null;
+}
+
+export const BlogDeatils = ({
+    authorName,
+    userDp
+} : BlogDeatilsType) => {
+
+    return  <div className="p-2 grid grid-cols-1 md:grid-cols-12 mx-2 md:mx-4 lg:mx-18 space-x-4 md:space-x-6 lg:space-x-20 space-y-6 md:space-y-0 ">
+                <div className=" p-4 md:col-span-8 space-y-5 md:space-y-2 lg:space-y-3  ">
+                        <div className="font-bold text-5xl  md:text-3xl lg:text-5xl tracking-tight leading-[55px] md:leading-[45px] lg:leading-[55px]">
+                            Taxing Laughter: The Joke Tax Chronicles
+                        </div>
+                        <div className=" text-slate-600 font-[400] tracking-tight text-base">
+                            Posted on August 24, 2023
+                        </div>
+                        <div className=" tracking-tight leading-7 md:leading-6 lg:leading-7 text-[17px] text-slate-900">
+                            Once upon a time, in a far-off land, there was a very lazy king who spent all day lounging on his throne. One day, his advisors came to him with a problem: the kingdom was running out of money.
+                            Jokester began sneaking into the castle in the middle of the night and leaving jokes all over the place: under the king's pillow, in his soup, even in the royal toilet. The king was furious, but he couldn't seem to stop Jokester.
+                            And then, one day, the people of the kingdom discovered that the jokes left by Jokester were so funny that they couldn't help but laugh. And once they started laughing, they couldn't stop.
+                        </div>
+                </div>
+                <div className="md:col-span-4 space-y-2 md:space-y-0 space-x-1 md:space-x-2 lg:space-y-3 p-4 ">
+                        <div className=" text-lg text-slate-950 font-[400] ">
+                            Author
+                        </div>
+                        <div className="flex space-x-2 md:space-x-4">
+                            <div className="flex flex-col justify-center">
+                                <MultiUserAvatar authorName={authorName} userDp={userDp}/>
+                            </div>
+                            <div className="flex flex-col justify-center text-[22px] md:text-2xl font-semibold tracking-tight">
+                                Jokestar
+                            </div>
+                        </div>
+                        <div className=" lg:ml-4 text-slate-600 font-[400]">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium accusamus illo aut vel
+                        </div>
+                </div>
+            </div>
+}
---
+++ frontend/src/components/BlogDetailSkeleton.tsx
@@ -0,0 +1,53 @@
+
+export const BlogDetailSkeleton = () => {
+    return <div className="p-2 grid grid-cols-1 md:grid-cols-12 mx-2 md:mx-4 lg:mx-18 space-x-4 md:space-x-6 lg:space-x-20 space-y-4 md:space-y-0">
+                <div className="p-4 md:col-span-8 space-y-5 md:space-y-2 lg:space-y-3 ">
+                    <div role="status" className=" animate-pulse  p-4 space-y-4 ">
+                        <div className="h-7 w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                        <div className="space-y-6 p-1">
+                                <div className="space-y-2">
+                                        <div className="h-4  w-1/3    bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4 w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                </div>
+                                <div className="space-y-2 ">
+                                        <div className="h-4  w-1/3    bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4 w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                </div>
+                                <div className="space-y-2 ">
+                                        <div className="h-4  w-1/3    bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4 w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                </div>
+                                                                <div className="space-y-2 ">
+                                        <div className="h-4  w-1/3    bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4 w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                </div>
+                                <div className="space-y-2 ">
+                                        <div className="h-4  w-1/3    bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4  w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                        <div className="h-4 w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                                </div>
+    
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="md:col-span-4 space-y-2 md:space-y-0 space-x-1 md:space-x-2 lg:space-y-3 p-4">
+                    <div role="status" className=" animate-pulse p-4 space-y-3 ">
+                        <div className="h-4 w-1/3 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                        <div className="h-4 w-1/2    bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                        <div className="h-4 w-4/5 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                        <div className="h-4 w-3/4 bg-gray-200 rounded-lg dark:bg-gray-300  flex flex-col justify-center"></div>
+                    </div>
+                </div>
+</div>
+}
